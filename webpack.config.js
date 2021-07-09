76b90e34e37bdd7b9ed7d5a88d49f1c133d5c938
Ensemble-Studio <69461395+Ensemble-Studio@users.noreply.github.com>
2021-07-09 09:51:40 +0200

    man

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 module.exports = {
+  output: {
+    publicPath: 'auto',
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+
       },
+
     ],
+
   },
 };
